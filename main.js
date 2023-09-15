6dec2b14bb2ce672ec8ca85689035aa4cc69ce2b
Dave-deJong <dndj81@yahoo.co.uk>
2023-09-15 14:21:36 +0100

    game till final alert

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ if (playerChoice === "1") {
 
 }
 else if (playerChoice === "2") {
+    player1 = prompt("Enter Your Name", "Enter Name")
     player2 = prompt("Enter Your Name", "Enter Name")
-player1 = prompt("Enter Your Name", "Enter Name")
+
 } 
 
 //function play(playerChoice, player){
@@ -22,6 +23,29 @@ player1 = prompt("Enter Your Name", "Enter Name")
 
 
 player1Choice = prompt(`${player1}, enter your Selection`, '1. rock, 2. paper 3.sissores')
-
+if(player1Choice === 1) {
+    let firstChoice = "Rock"
+}else if (player1Choice === 2) {
+    let firstChoice = "Paper"
+}else(player1Choice === 3); {
+    let firstChoice = "Scissors"
+}
 
 player2Choice = prompt(`${player2}, enter your Selection`, '1. rock, 2. paper 3.sissores')
+if(player2Choice === 1) {
+    let secondChoice = "Rock"
+}else if (player2Choice === 2) {
+    let secondChoice = "Paper"
+}else(player2Choice === 3); {
+    let secondChoice = "Scissors"
+}
+
+if (player1Choice === player2Choice){
+    alert ("Game is tied")
+}
+else if((player1Choice === 1 && player2Choice === 3)||(player1Choice === 2 && player2Choice === 1)||(player1Choice === 3 && player2Choice === 2)){
+    alert (`${player1} wins! ${player1}'s ${firstChoice} beats ${player2}'s ${secondChoice}`)
+}
+else {
+    alert (`${player2} wins!${player2}'s ${secondChoice} beats ${player1}'s ${firstChoice}`)
+}
