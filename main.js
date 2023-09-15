4961fc8800ab7066832c42c3ffe44b8040942e55
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-09-15 12:30:47 +0100

    rock

--- main.js
+++ main.js
@@ -6,12 +6,22 @@ let computer;
 
 playerChoice = prompt("Select One or Two Player Mode...", "enter 1 or 2")
 if (playerChoice === "1") {
-    player1 = prompt("Enter Your Name", "Enter Name")
+    player1 = prompt("Enter Your Name", "Enter Name") 
+
 }
 else if (playerChoice === "2") {
     player2 = prompt("Enter Your Name", "Enter Name")
 player1 = prompt("Enter Your Name", "Enter Name")
 } 
-function play(playerChoice, player){
-playerChoice = prompt("Select One or Two Player Mode...", "enter 1 or 2")
-}
+
+//function play(playerChoice, player){
+    const choices= ['rock', 'paper', 'sissores'];
+   // const compChoice= choice[Math.floor(Math.random() * 3)];
+   // console.log(choices[0]) 
+//}
+
+
+player1Choice = prompt(`${player1}, enter your Selection`, '1. rock, 2. paper 3.sissores')
+
+
+player2Choice = prompt(`${player2}, enter your Selection`, '1. rock, 2. paper 3.sissores')
