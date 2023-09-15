e64f0601a51bd2ceab6efd9790ec6d99d7f5aca6
melvinJD <114363341+melvinJD@users.noreply.github.com>
2023-09-15 12:03:46 +0100

    player 1 2 option

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+let player1;
+let player2;
+let computer;
+
+playerChoice = prompt("Select One or Two Player Mode...", "enter 1 or 2")
+if (playerChoice === "1") {
+    player1 = prompt("Enter Your Name", "Enter Name")
+}
+else if (playerChoice === "2") {
+    player2 = prompt("Enter Your Name", "Enter Name")
+player1 = prompt("Enter Your Name", "Enter Name")
+} 
+function play(playerChoice, player){
+playerChoice = prompt("Select One or Two Player Mode...", "enter 1 or 2")
+}
